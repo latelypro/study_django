89c366ee263b2243fedf6bcc6d66c7dee137ecac
unknown <d.uchida39@gmail.com>
2018-12-03 22:46:05 +0900

    fix: スペルミス修正

--- django_tutorial/mysite/polls/models.py
+++ django_tutorial/mysite/polls/models.py
@@ -19,14 +19,13 @@ class Question(models.Model):
         --------
         True : 1日以内, False: 1日以上前
         """
-
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # choiseクラスをQuestionクラスに関連付け。1：1
-    choise_text = models.CharField(max_length=20)
+    choice_text = models.CharField(max_length=20)
     votes = models.IntegerField(default=0) # 整数型変数
 
     def __str__(self):
