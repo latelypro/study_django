57414dab2ee5d7557a0e6251a6adb05c6da76a37
duchida <d.uchida39@gmail.com>
2018-12-19 20:26:37 +0900

    update: extends template

--- DJANGO_for_BEGINNERS/pages/pages/urls.py
+++ DJANGO_for_BEGINNERS/pages/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView # View Classのimport
+from .views import HomePageView , AboutPageView# View Classのimport
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'), # これでサーバー接続時にViewClassで指定したtemplateが表示されるようになる
+    path('about/', AboutPageView.as_view(), name='about'), 
 ]
