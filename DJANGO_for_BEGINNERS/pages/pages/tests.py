eb64be2162ea23ac72068888b47a918a66ec23d3
duchida <d.uchida39@gmail.com>
2018-12-20 19:59:51 +0900

    test: HttpRequestの応答確認テスト追加

--- DJANGO_for_BEGINNERS/pages/pages/tests.py
+++ DJANGO_for_BEGINNERS/pages/pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+class SimpleTest(SimpleTestCase):
+    
+    # HttpRequestに対してリクエスト成功(=200)の応答が返ってきていることを確認するテスト
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200) 
+    
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
